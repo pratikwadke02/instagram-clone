a3b57ff21e6c95ac8c9e43c3edb6f43c00eefbb7
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-05-06 01:47:39 +0530

    added Profile Screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/screens/add_post_screen.dart';
 import 'package:instagram/screens/feed_screen.dart';
+import 'package:instagram/screens/profile_screen.dart';
 import 'package:instagram/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('activity'),
-  Text('profile'),
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
